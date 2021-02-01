5195cc50cb0a59321d303bb879f02c6f333dfdc5
Atabaev <blackpanda7@mail.ru>
2021-02-01 18:31:22 +0600

    Chat

---
+++ main.js
@@ -0,0 +1,31 @@
+const button = document.querySelector('button')
+const input = document.querySelector('#text')
+const chat = document.querySelector('.chat')
+input.focus()
+
+button.onclick = (e) => {
+    const message = input.value
+    if (message.trim().length > 0){
+        const spanSend = document.createElement('span')
+        spanSend.className = 'send'
+        spanSend.innerText = message
+        chat.appendChild(spanSend)
+        const spanReceive = document.createElement('span')
+        spanReceive.className = 'receive'
+        spanReceive.innerText = 'typing...'
+        chat.appendChild(spanReceive)
+        setTimeout (() =>{
+            if (message === 'kandai'){
+                spanReceive.innerText = 'good'
+            }else if (message === 'what dod you do?'){
+                spanReceive.innerText = 'sitting'
+            }else{
+                spanReceive.innerText = 'Whaat ?'
+            }
+        }, 2000)
+        input.value = ''
+        input.focus()
+
+
+    }
+}
